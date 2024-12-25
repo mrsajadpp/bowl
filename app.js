d6aba927e1dae3a3170efc38a98325ee86b10f00
mrsajadpp <mrsajadpp@gmail.com>
2024-12-25 22:09:02 +0530

    fix: redirect users to the home page instead of login when already logged in

--- app.js
+++ app.js
@@ -30,7 +30,7 @@ function checkLoggedIn(req, res, next) {
     if (!req.session.user) {
         return next();
     }
-    return res.redirect('/auth/login');
+    return res.redirect('/');
 }
 
 // Middleware to check if user is not logged in
