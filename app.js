e283bd591cec211595e1cb502a90daa5b04a5a94
mrsajadpp <mrsajadpp@gmail.com>
2024-12-25 17:49:22 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const path = require('path');
+
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+// Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+// Static files
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Routes
+const indexRouter = require('./routes/index');
+app.use('/', indexRouter);
+
+// Database connection
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log('MongoDB connected'))
+    .catch(err => console.log(err));
+
+// Start server
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
