61ebb793f6d7fa703bc1368d7d934c34c89d6f1c
mrsajadpp <mrsajadpp@gmail.com>
2024-12-25 17:54:38 +0530

    refactor database connection to simplify mongoose.connect options

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ const indexRouter = require('./routes/index');
 app.use('/', indexRouter);
 
 // Database connection
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.MONGO_URI)
     .then(() => console.log('MongoDB connected'))
     .catch(err => console.log(err));
 
