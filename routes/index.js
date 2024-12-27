e00bd45dae563650852915addf6c8cd238df5edb
mrsajadpp <mrsajadpp@gmail.com>
2024-12-27 10:07:28 +0530

    privacy poicy added

--- routes/index.js
+++ routes/index.js
@@ -24,5 +24,23 @@ router.get('/about', async (req, res) => {
     }
 });
 
+router.get('/terms-and-conditions', async (req, res) => {
+    try {
+        res.status(500).render('index/terms_and_conditions', { title: 'Bowl.', layout: 'index_layout', error: null, message: null, auth_page: true });
+    } catch (err) {
+        console.error(err);
+        res.status(500).render('index/terms_and_conditions', { title: 'Bowl.', error: 'Server error', message: null, auth_page: true });
+    }
+});
+
+router.get('/privacy-policy', async (req, res) => {
+    try {
+        res.status(500).render('index/privacy_policy', { title: 'Bowl.', layout: 'index_layout', error: null, message: null, auth_page: true });
+    } catch (err) {
+        console.error(err);
+        res.status(500).render('index/privacy_policy', { title: 'Bowl.', error: 'Server error', message: null, auth_page: true });
+    }
+});
+
 
 module.exports = router;
