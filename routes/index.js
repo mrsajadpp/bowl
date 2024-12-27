70c2eb4882590bd3a1dd52759bc7f4f592ac6f10
mrsajadpp <mrsajadpp@gmail.com>
2024-12-27 16:45:20 +0530

    bug fixed

--- routes/index.js
+++ routes/index.js
@@ -8,7 +8,7 @@ const { default: mongoose } = require('mongoose');
 
 router.get('/', async (req, res) => {
     try {
-        res.status(500).render('index/index', {
+        res.render('index/index', {
             layout: 'index_layout',
             title: 'Bowl - Manage Your Finances Easily',
             metaDescription: 'Bowl helps you manage your finances effortlessly. Track your income and expenses with ease, and make smarter financial decisions.',
@@ -26,7 +26,7 @@ router.get('/', async (req, res) => {
 
 router.get('/about', async (req, res) => {
     try {
-        res.status(500).render('index/about', {
+        res.render('index/about', {
             title: 'About Bowl - Learn More About Us',
             metaDescription: 'Learn more about Bowl, the financial management platform designed to help you track and optimize your spending and income.',
             layout: 'index_layout', error: null, message: null, auth_page: true, req: req
@@ -43,7 +43,7 @@ router.get('/about', async (req, res) => {
 
 router.get('/terms-and-conditions', async (req, res) => {
     try {
-        res.status(500).render('index/terms_and_conditions', {
+        res.render('index/terms_and_conditions', {
             title: 'Terms and Conditions - Bowl',
             metaDescription: 'Read the terms and conditions for using the Bowl platform. These terms govern your use of our website and financial management services.',
             layout: 'index_layout', error: null, message: null, auth_page: true, req: req
@@ -60,7 +60,7 @@ router.get('/terms-and-conditions', async (req, res) => {
 
 router.get('/privacy-policy', async (req, res) => {
     try {
-        res.status(500).render('index/privacy_policy', {
+        res.render('index/privacy_policy', {
             title: 'Privacy Policy - Bowl',
             metaDescription: 'Read Bowl’s privacy policy to understand how we collect, use, and protect your personal information while using our financial management platform.',
             layout: 'index_layout', error: null, message: null, auth_page: true, req: req
